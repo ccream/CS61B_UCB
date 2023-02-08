8291e7905fd707a733c0ae0e94f0971a391239d1
Jiasen Liu <jliu3566@usc.edu>
2023-02-07 20:49:49 -0800

    Update OffByN.java

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,5 +1,12 @@
-package Project1B;
-
 public class OffByN {
-
+  int N;
+	
+	public OffByN(int N)  {
+		this.N = N;
+	}
+	
+	@Override
+	public boolean equalChars(char x, char y)  {
+		return Math.abs(x-y) == N;
+	}
 }
