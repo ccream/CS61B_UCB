e5fbf72159f5c1242ccf0009e9b331a4bb9ee854
Jiasen Liu <jliu3566@usc.edu>
2023-02-13 00:11:43 -0800

    Update TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,8 +14,10 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'c'));
         assertFalse(offByOne.equalChars('a', 'A'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('6', '8'));
         assertTrue(offByOne.equalChars('&', '%'));
         assertTrue(offByOne.equalChars('b', 'a'));
+        assertTrue(offByOne.equalChars('6', '5'));
     }
     
     //Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
